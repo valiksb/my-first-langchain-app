14c35b0648c0266b5d552c0b44470fb92e750e30
Valik Solorzano <vsolorzanobarboza@zebra.com>
2023-04-26 20:23:34 -0400

    chore: added a second chain and connected them

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
+from langchain.chains import LLMChain
 
 openai = OpenAI(temperature=0.9)
 template = """Answer the question based on the context below. If the
@@ -9,25 +10,28 @@ with "I don't know".
 
 Context:
 
-Question: {query}
+Question: What would be a good company name for a company that makes {product}?
 
 Answer: """
 
-#print(llm(prompt))
-
 prompt_template = PromptTemplate(
-    input_variables=["query"],
+    input_variables=["product"],
     template=template
 )
 
-print(
-    prompt_template.format(
-        query="What would be a good company name for a company that makes colorful socks?"
-    )
+chain = LLMChain(llm=openai, prompt=prompt_template)
+#print(chain.run("colorful socks"))
+
+second_prompt = PromptTemplate(
+    input_variables=["company_name"],
+    template="Write a catchphrase for the following company: {company_name}",
 )
 
-print(openai(
-    prompt_template.format(
-        query="What would be a good company name for a company that makes colorful socks?"
-    )
-))
+chain_two = LLMChain(llm=openai, prompt=second_prompt)
+
+from langchain.chains import SimpleSequentialChain
+overall_chain = SimpleSequentialChain(chains=[chain, chain_two], verbose=True)
+
+# Run the chain specifying only the input variable for the first chain.
+catchphrase = overall_chain.run("colorful socks")
+print(catchphrase)
